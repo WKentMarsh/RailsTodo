3ea937437011011ff80b88a4bf5bd4ee9aedd5d7
Kent Marsh <Kent.Marsh@agilethought.com>
2022-05-16 15:38:09 -0400

    Refactored, removed redundant html

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,51 +1,53 @@
 class TodosController < ApplicationController
- def new
-    @todo = Todo.new
- end
+    before_action :set_todo, only: [:edit, :update, :show, :destroy]
 
- def create
-    @todo = Todo.new
-
-    if @todo.save(todo_params)
-        flash[:notice] = "Todo was created succesfully"
-        redirect_to todo_path(@todo)
-    else
-        render 'new'
+    def new
+        @todo = Todo.new
     end
- end
 
- def show
-    @todo = Todo.find(params[:id])
- end
+    def create
+        @todo = Todo.new(todo_params)
 
- def edit
-    @todo = Todo.find(params[:id])
- end 
-
- def update
-    @todo = Todo.find(params[:id])
-
-    if @todo.update(todo_params)
-        flash.notice = "Todo was succesfully updated"
-        redirect_to todo_path(@todo)
-    else
-        render 'edit'
+        if @todo.save
+            flash[:notice] = "Todo was created succesfully"
+            redirect_to todo_path(@todo)
+        else
+            render 'new'
+        end
     end
- end
 
- def index
-    @todos = Todo.all
- end
-
- def destroy
-    @todo = Todo.find(params[:id])
-    @to.destroy
-    flash[:notice] = "Todo was deleted successfully."
-    redirect_to todo_path
- end 
-
- private
-    def todo_params
-    params.require(:todo).permit(:name, :description)
+    def show
+        set_todo
     end
+
+    def edit
+    end 
+
+    def update
+        if @todo.update(todo_params)
+            flash.notice = "Todo was succesfully updated"
+            redirect_to todo_path(@todo)
+        else
+            render 'edit'
+        end
+    end
+
+    def index
+        @todos = Todo.all
+    end
+
+    def destroy
+        @todo.destroy
+        flash[:notice] = "Todo was deleted successfully."
+        redirect_to todo_path
+    end 
+
+    private
+        def set_todo
+            @todo = Todo.find(params[:id])
+        end
+        def todo_params
+            params.require(:todo).permit(:name, :description)
+        end
+
 end
